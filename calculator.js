d715a5591a673f7093a6d50faf84473ae32d4048
raissa <raissavml@hotmail.com>
2024-02-14 22:28:22 -0300

    first commit

---
+++ calculator.js
@@ -0,0 +1,7 @@
+const calculator = document.querySelector('calculator');
+const funtionalities = caculator.querySelector('functionalities');
+funtionalities.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        
+    }
+})
